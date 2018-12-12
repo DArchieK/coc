776db54eff1da689b49fc1db8fd9b08e72e0b7fe
DAK <fjboy08@163.com>
2018-12-12 20:37:23 +0800

    init coc

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Character from '@/components/Character'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Character',
+      component: Character
+    }
+  ]
+})
